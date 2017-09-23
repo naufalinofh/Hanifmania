1037749625bd7acfd0880b499b2b86c31404b031
naufalinofh <impulsboulevard@gmail.com>
2017-09-23 14:54:59 +0700

    adding batch file for simulation purpose

---
+++ simulation.bat
@@ -0,0 +1,4 @@
+start C:\Users\user\Documents\"Mission Planner"\sitl\Arducopter.exe -M+ -s1 --uartA tcp:0 --instance 1 --home -6.9012803,107.6188396,740,10
+start  C:\Users\user\Documents\"Mission Planner"\sitl\Arduplane.exe -Mplane -s1 --uartA tcp:0 --instance 2 --home -6.9012803,107.6188396,740,0
+
+pause
